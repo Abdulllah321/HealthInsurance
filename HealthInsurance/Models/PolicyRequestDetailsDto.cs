9c52f38ebb56e80cdc41aeea6ee9efb77b0dbe94
Abdulllah321 <abdullahsufyan2007@gmail.com>
2024-09-17 01:04:08 +0500

    policies Request added

---
+++ HealthInsurance/Models/PolicyRequestDetailsDto.cs
@@ -0,0 +1,26 @@
+﻿using HealthInsurance.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace HealthInsurance.Models
+{
+    public class PolicyRequestDetailsDto
+    {
+        [Key]
+        public int RequestId { get; set; }
+
+        public DateTime RequestDate { get; set; }
+
+        public int EmpNo { get; set; }
+        
+        public int PolicyId { get; set; }
+
+        public decimal PolicyAmount { get; set; }
+        public decimal EMI { get; set; }
+
+        public int CompanyId { get; set; }
+
+        [StringLength(50)]
+        public string? Status { get; set; }
+    }
+}
